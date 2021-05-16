09d7631919017f70bb61221e7d00e7aea30cc1c3
sweettsubaki <genmashika@hotmail.fr>
2021-05-16 21:44:59 +0200

    a commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,11 @@ class User < ApplicationRecord
   has_many :comments
   has_many :sent_messages, foreign_key: 'sender_id', class_name: "PrivateMessage"
   has_many :received_messages, foreign_key: 'recipient_id', class_name: "PrivateMessage"
-  belongs_to :city, optional: true
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :city, presence: true
+  
   validates :age, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }, presence: true
   validates :description, presence: true
   
